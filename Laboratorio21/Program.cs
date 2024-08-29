a4bcc2d88f2312694b18d1195303fb49703417c5
Stelar0720 <76462555+Stelar0720@users.noreply.github.com>
2024-08-28 19:39:02 -0500

    ejemplo 2

---
+++ Laboratorio21/Program.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualBasic;
+using System;
+
+namespace Laboratio21
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            //asignando valor a variable estatica.
+            MyClass.Valor = 1;
+            Console.WriteLine(MyClass.Valor);        
+        }
+    }
+    public class MyClass
+    {
+        //declarando variable estatica
+        public static int Valor;
+    }
+}
