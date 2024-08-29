c4240850f9df29101a8639b3c37b9a9963d02558
Stelar0720 <76462555+Stelar0720@users.noreply.github.com>
2024-08-28 19:26:54 -0500

    ejemplo 1

--- Laboratorio 2/Program.cs
+++ Laboratorio 2/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace Laboratorio_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Client client = new Client();
+            client.FirstName = "Su_Nombre";
+            client.LastName = "Su_Apellido";
+            client.Age = 15;
+            client.Id = 1;
+
+            Console.WriteLine(client.GetFullName());
+        }
+    }
+    public class Client
+    { 
+        //Declarando variables a instancia en clase.
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public ushort Age { get; set; }
+
+        public string GetFullName()
+        { 
+            //utilizando variables de instancia dentro de metodos de la clase.
+            return FirstName + " " + LastName;
+        }
+    }
+}
