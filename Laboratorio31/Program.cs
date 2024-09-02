6e7a341a25f46883361d7f29b238bc0f24d61f1c
Stelar0720 <76462555+Stelar0720@users.noreply.github.com>
2024-09-02 18:49:29 -0500

    laboratorio 31

---
+++ Laboratorio31/Program.cs
@@ -0,0 +1,25 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+
+        Console.Write("Introduce el primer número: ");
+        int primerNumero = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Introduce el segundo número: ");
+        int segundoNumero = Convert.ToInt32(Console.ReadLine());
+
+        double resultadoOperacion = CalculosMatematicos.Calcular(primerNumero, segundoNumero);
+        Console.WriteLine("El resultado de la operación (a+b)*(a-b) es: {0}", resultadoOperacion);
+    }
+}
+
+internal class CalculosMatematicos
+{
+    // Método para calcular (a+b)*(a-b)
+    public static double Calcular(double a, double b)
+    {
+        return (a + b) * (a - b);
+    }
+}
+
