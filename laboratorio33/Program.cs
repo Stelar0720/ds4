4743c2f7d2549179273928f1af5a6993f1c74bc3
Stelar0720 <76462555+Stelar0720@users.noreply.github.com>
2024-09-02 19:09:45 -0500

    laboratorio 33

---
+++ laboratorio33/Program.cs
@@ -0,0 +1,15 @@
+﻿internal class Program
+{
+    private static void Main(String[] args)
+    {
+
+        Console.Write("Introduce el lado largo del rectangulo: ");
+        double ladoLargo = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Introduce el lado corto del rectangulo: ");
+        double ladoCorto = Convert.ToDouble(Console.ReadLine());
+
+        double perimetroRectangulo = 2 * (ladoLargo + ladoCorto);
+        Console.WriteLine("El perimetro del rectangulo es: {0}", perimetroRectangulo);
+    }
+}
