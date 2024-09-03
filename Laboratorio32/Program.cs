e03527274a27e082a1b05468e89fce7030b924d6
Stelar0720 <76462555+Stelar0720@users.noreply.github.com>
2024-09-02 19:01:33 -0500

    laboratorio 32

--- Laboratorio32/Program.cs
+++ Laboratorio32/Program.cs
@@ -1,1 +1,19 @@
-﻿
+﻿internal class Program
+{
+    private static void Main(String[] args)
+    {
+        Console.Write("Introduce el radio del circulo: ");
+        double radio = Convert.ToDouble(Console.ReadLine());
+
+        double areaCirculo = CalculosMatematicos.CalculoArea(radio);
+        Console.WriteLine("El area del circulo es: {0}", areaCirculo);
+    }
+}
+
+internal class CalculosMatematicos
+{
+    public static double CalculoArea(double radio)
+    {
+        return 3.1416 * Math.Pow(radio, 2);    
+    }
+}
